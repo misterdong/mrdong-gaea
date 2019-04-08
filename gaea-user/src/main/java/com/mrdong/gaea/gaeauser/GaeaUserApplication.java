f528ce8d9841c388cc53a57ac876d740c6cd5970
misterdong <a895011874@qq.com>
2019-04-08 18:51:44 +0800

    dd

---
+++ gaea-user/src/main/java/com/mrdong/gaea/gaeauser/GaeaUserApplication.java
@@ -0,0 +1,13 @@
+package com.mrdong.gaea.gaeauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaeaUserApplication.class, args);
+    }
+
+}
