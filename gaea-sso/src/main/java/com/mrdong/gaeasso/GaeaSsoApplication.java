e353df3bda3f3df6f8ffc9686a7ba8530e80b875
misterdong <a895011874@qq.com>
2019-04-08 18:57:57 +0800

    dd

---
+++ gaea-sso/src/main/java/com/mrdong/gaeasso/GaeaSsoApplication.java
@@ -0,0 +1,13 @@
+package com.mrdong.gaeasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeaSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaeaSsoApplication.class, args);
+    }
+
+}
